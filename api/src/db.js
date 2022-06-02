4cca9c7d36501b0ed6b8f08470ebbc223a596466
fvaron69 <fredyalberbaron@hotmail.com>
2022-06-02 10:56:05 -0500

    add nPORT

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
@@ -30,13 +28,10 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
         { logging: false, native: false }
       );
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-//   // logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
